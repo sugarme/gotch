c45ca32070d080c9aa37d50cb49c6a34de3a52f1
sugarme <thangtran@hotmail.com>
2020-05-26 02:40:37 +1000

    lib.go clean up cgo flags

--- torch/lib.go
+++ torch/lib.go
@@ -1,6 +1,6 @@
 package torch
 
-// #cgo CXXFLAGS: -std=c++14 -I${SRCDIR} -O3 -Wall -g -Wno-sign-compare -Wno-unused-function -I/Library/Developer/CommandLineTools/usr/include/c++/v1 -I/usr/local/include -I/opt/libtorch/include -I/opt/libtorch/include/torch/csrc/api/include
-// #cgo LDFLAGS: -lstdc++ -L/opt/libtorch/lib  -ltorch -lcaffe2 -lc10
+// #cgo CXXFLAGS: -std=c++14 -I${SRCDIR} -O3 -Wall -g -Wno-sign-compare -Wno-unused-function -I/usr/local/include -I/opt/libtorch/include -I/opt/libtorch/include/torch/csrc/api/include
+// #cgo LDFLAGS: -L/opt/libtorch/lib -ltorch
 // #cgo CFLAGS: -D_GLIBCXX_USE_CXX11_ABI=1
 import "C"
