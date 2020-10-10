d55436e1ea3bfa39e951a461f1e05395341be8a3
sugarme <thangtran@hotmail.com>
2020-10-10 21:17:58 +1100

    update GPU script

--- setup-libtorch-gpu-cu101.sh
+++ setup-libtorch-gpu-cu101.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo rm -rf /opt/libtorch
-wget -O /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1+cpu.zip https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcpu.zip
-unzip /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1+cpu.zip -d /opt
-sudo echo LD_LIBRARY_PATH=/opt/libtorch/lib:/usr/lib64-nvidia:/usr/local/cuda-10.1/lib64 >> $HOME/.bashrc
+export GOTCH_VERSION="v0.1.0"
+export LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@{GOTCH_VERSION}/libtch"
+export LIBRARY_PATH=$LIBTORCH/lib
+export CPATH=$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include
+export LD_LIBRARY_PATH=$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-10.1/lib64
+
+sudo rm -rf $LIBTORCH
+mkdir -p $LIBTORCH
+wget -O /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcu101.zip https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcu101.zip
+unzip /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcu101.zip -d $LIBTORCH
+
