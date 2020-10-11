f455029f08a7c2262d5b8f75ae5b7b0a1d387643
sugarme <thangtran@hotmail.com>
2020-10-11 11:27:28 +1100

    updated scripts

--- setup-libtorch-cpu.sh
+++ setup-libtorch-cpu.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 # Env
-export GOTCH_VERSION="v0.1.2"
-export LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch"
-export LIBRARY_PATH=$LIBTORCH/lib
-export CPATH=$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc:$LIBTORCH/include/torch/csrc/api/include
-export LD_LIBRARY_PATH=$LIBTORCH/lib:$LD_LIBRARY_PATH
+GOTCH_VERSION="v0.1.3"
+GOTCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION"
+LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch/libtorch"
+LIBRARY_PATH="$LIBTORCH/lib"
+CPATH="$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc:$LIBTORCH/include/torch/csrc/api/include"
+LD_LIBRARY_PATH="$LIBTORCH/lib"
 
 # Precompiled libtorch
+sudo rm -rf $LIBTORCH
+sudo mkdir -p $LIBTORCH
 wget -O /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1+cpu.zip https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcpu.zip
 sudo unzip /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1+cpu.zip -d $LIBTORCH
 
 # Update .bashrc
-export FILE='$HOME/.bashrc'
-export LN_LIBTORCH='export LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch"'
-export LN_LIBRARY_PATH='export LIBRARY_PATH=$LIBTORCH/lib'
-export LN_CPATH='export CPATH=$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc:$LIBTORCH/include/torch/csrc/api/include'
-export LN_LD_LIBRARY_PATH = 'export LD_LIBRARY_PATH=$LIBTORCH/lib:$LD_LIBRARY_PATH'
+FILE="$HOME/.bashrc"
+LN_LIBTORCH="export LIBTORCH=$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch/libtorch"
+LN_LIBRARY_PATH="export LIBRARY_PATH=$LIBTORCH/lib"
+LN_CPATH="export CPATH=$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc:$LIBTORCH/include/torch/csrc/api/include"
+LN_LD_LIBRARY_PATH="export LD_LIBRARY_PATH=$LIBTORCH/lib:$LD_LIBRARY_PATH"
 sudo grep -qF -- "$LN_LIBTORCH" "$FILE" || sudo echo "$LN_LIBTORCH" >> "$FILE"
 sudo grep -qF -- "$LN_LIBRARY_PATH" "$FILE" || sudo echo "$LN_LIBRARY_PATH" >> "$FILE"
 sudo grep -qF -- "$LN_CPATH" "$FILE" || sudo echo "$LN_CPATH" >> "$FILE"
 sudo grep -qF -- "$LN_LD_LIBRARY_PATH" "$FILE" || sudo echo "$LN_LD_LIBRARY_PATH" >> "$FILE"
 
-# Update CPU config
-sudo rm $LIBTORCH/lib.go
-sudo cp $LIBTORCH/lib.go.cpu $LIBTORCH/lib.go
-sudo mv $LIBTORCH/dummy_cuda_dependency.cpp $LIBTORCH/dummy_cuda_dependency.cpp.gpu
-sudo mv $LIBTORCH/fake_cuda_dependency.cpp.cpu $LIBTORCH/fake_cuda_dependency.cpp
+sudo rm $GOTCH/libtch/lib.go
+sudo cp $GOTCH/libtch/lib.go.cpu $GOTCH/libtch/lib.go
+sudo mv $GOTCH/libtch/dummy_cuda_dependency.cpp $GOTCH/libtch/dummy_cuda_dependency.cpp.gpu
+sudo mv $GOTCH/libtch/fake_cuda_dependency.cpp.cpu $GOTCH/libtch/fake_cuda_dependency.cpp
+
+source "$FILE"
--- setup-libtorch-gpu-cu101.sh
+++ setup-libtorch-gpu-cu101.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-export GOTCH_VERSION="v0.1.2"
-export LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch"
-export LIBRARY_PATH=$LIBTORCH/lib
-export CPATH=$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-10.1/lib64
+GOTCH_VERSION="v0.1.3"
+GOTCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION"
+LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch/libtorch"
+LIBRARY_PATH="$LIBTORCH/lib"
+CPATH="$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include"
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-10.1/lib64"
+
+sudo rm -rf $LIBTORCH
+sudo mkdir -p $LIBTORCH
 
 wget -O /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcu101.zip https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcu101.zip
 sudo unzip /tmp/libtorch-cxx11-abi-shared-with-deps-1.5.1%2Bcu101.zip -d $LIBTORCH
 
 # update .bashrc
-export FILE='$HOME/.bashrc'
-export LN_LIBTORCH='export LIBTORCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch"'
-export LN_LIBRARY_PATH='export LIBRARY_PATH=$LIBTORCH/lib'
-export LN_CPATH='export CPATH=$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include'
-export LN_LD_LIBRARY_PATH='export LD_LIBRARY_PATH=$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-10.1/lib64'
+FILE="$HOME/.bashrc"
+LN_LIBTORCH="export LIBTORCH=$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION/libtch/libtorch"
+LN_LIBRARY_PATH="export LIBRARY_PATH=$LIBTORCH/lib"
+LN_CPATH="export CPATH=$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include"
+LN_LD_LIBRARY_PATH="export LD_LIBRARY_PATH=$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-10.1/lib64"
 sudo grep -qF -- "$LN_LIBTORCH" "$FILE" || sudo echo "$LN_LIBTORCH" >> "$FILE"
 sudo grep -qF -- "$LN_LIBRARY_PATH" "$FILE" || sudo echo "$LN_LIBRARY_PATH" >> "$FILE"
 sudo grep -qF -- "$LN_CPATH" "$FILE" || sudo echo "$LN_CPATH" >> "$FILE"
 sudo grep -qF -- "$LN_LD_LIBRARY_PATH" "$FILE" || sudo echo "$LN_LD_LIBRARY_PATH" >> "$FILE"
+
+source "$FILE"
