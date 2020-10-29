59ea5f0e1b0177ff68534b0185331c9b1d765abc
sugarme <thangtran@hotmail.com>
2020-10-29 23:46:40 +1100

    updated scripts

--- setup-cpu.sh
+++ setup-cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Env
-GOTCH_VERSION="${GOTCH_VER:-v0.1.9}"
+GOTCH_VERSION="${GOTCH_VER:-v0.1.10}"
 LIBTORCH_VERSION="${LIBTORCH_VER:-1.5.1}"
 
 GOTCH="$GOPATH/pkg/mod/github.com/sugarme/gotch@$GOTCH_VERSION"
