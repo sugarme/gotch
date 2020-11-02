d2ad0e7f5a7416ae833f0e803fdaf606995af140
sugarme <thangtran@hotmail.com>
2020-11-03 00:26:25 +1100

    Updated CHANGELOG, README and scripts for v0.3.0

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-GOTCH_VERSION="${GOTCH_VER:-v0.2.1}"
-LIBTORCH_VERSION="${LIBTORCH_VER:-1.7.0}"
-CUDA_VERSION="${CUDA_VER:-10.1}"
-CU_VERSION="${CUDA_VERSION//./}"
+export GOTCH_VERSION="${GOTCH_VER:-v0.3.0}"
+export LIBTORCH_VERSION="${LIBTORCH_VER:-1.7.0}"
+export CUDA_VERSION="${CUDA_VER:-10.1}"
+export CU_VERSION="${CUDA_VERSION//./}"
 
 export GOTCH="$HOME/projects/sugarme/gotch"
 export LIBTORCH="$HOME/projects/sugarme/gotch/libtch/libtorch"
 export LIBRARY_PATH="$LIBTORCH/lib"
-export CPATH="$CPATH:$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-${CUDA_VERSION}/lib64"
+export CPATH="$LIBTORCH/lib:$LIBTORCH/include:$LIBTORCH/include/torch/csrc/api/include"
+export LD_LIBRARY_PATH="$LIBTORCH/lib:/usr/lib64-nvidia:/usr/local/cuda-${CUDA_VERSION}/lib64"
 
 sudo rm -rf $LIBTORCH
 sudo mkdir -p $LIBTORCH
