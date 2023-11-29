983939a9b8e3f41ba512e8403646df1bafd57909
sugarme <thangtran@hotmail.com>
2023-11-29 20:55:15 +1100

    clean up cgo flags

--- setup-gotch.sh
+++ setup-gotch.sh
@@ -45,10 +45,7 @@ package libtch
 // #cgo CFLAGS: -I/usr/local/include
 // #cgo CFLAGS: -D_GLIBCXX_USE_CXX11_ABI=1
 // #cgo LDFLAGS: -lstdc++ -ltorch -lc10 -ltorch_cpu -L/lib64
-// #cgo CXXFLAGS: -std=c++17 -I${SRCDIR} -g -O3
-// #cgo CFLAGS: -I${SRCDIR}/libtorch/lib -I${SRCDIR}/libtorch/include -I${SRCDIR}/libtorch/include/torch/csrc/api/include -I${SRCDIR}/libtorch/include/torch/csrc
-// #cgo LDFLAGS: -L${SRCDIR}/libtorch/lib
-// #cgo CXXFLAGS: -I${SRCDIR}/libtorch/lib -I${SRCDIR}/libtorch/include -I${SRCDIR}/libtorch/include/torch/csrc/api/include -I${SRCDIR}/libtorch/include/torch/csrc
+// #cgo CXXFLAGS: -std=c++17
 import "C"
 EOT
 else
@@ -56,12 +53,14 @@ else
   sudo tee -a $GOTCH_LIB_FILE > /dev/null <<EOT
 package libtch
 
-// #cgo LDFLAGS: -lstdc++ -ltorch -lc10 -ltorch_cpu
-// #cgo LDFLAGS: -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcudnn -lcaffe2_nvrtc -lnvrtc-builtins -lnvrtc -lnvToolsExt -lc10_cuda -ltorch_cuda
+package libtch
+
 // #cgo CFLAGS: -I${SRCDIR} -O3 -Wall -Wno-unused-variable -Wno-deprecated-declarations -Wno-c++11-narrowing -g -Wno-sign-compare -Wno-unused-function
-// #cgo CFLAGS: -D_GLIBCXX_USE_CXX11_ABI=1
 // #cgo CFLAGS: -I/usr/local/cuda/include
-// #cgo CXXFLAGS: -std=c++17 -I${SRCDIR} -g -O3
+// #cgo LDFLAGS: -lstdc++ -ltorch -ltorch_cpu -ltorch_cuda -lc10
+// #cgo LDFLAGS: -L/usr/local/cuda/lib64 -lcuda
+// #cgo CFLAGS: -D_GLIBCXX_USE_CXX11_ABI=1
+// #cgo CXXFLAGS: -std=c++17
 import "C"
 EOT
 fi
